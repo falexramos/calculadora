39a4b38000e97544f85e8c3888798ea7fe97f7e4
favian <framos.ext@alianza.com.co>
2023-05-09 13:24:22 +0200

    se agrega la dependencia de la libreria Trace y se utiliza en el impl

--- calculadora/src/main/java/com/calculadora/service/impl/CalculosServiceImpl.java
+++ calculadora/src/main/java/com/calculadora/service/impl/CalculosServiceImpl.java
@@ -1,14 +1,19 @@
 package com.calculadora.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.calculadora.dto.RespuestaMensajeDTO;
 import com.calculadora.service.CalculosService;
+import io.corp.calculator.TracerImpl;
 
 @Service
 public class CalculosServiceImpl implements CalculosService {
-
+	
+	@Autowired
+	private TracerImpl tracer;
+	
 	@Override
 	public RespuestaMensajeDTO suma(String a, String b) {
 		RespuestaMensajeDTO respuestaMensajeDTO = new RespuestaMensajeDTO();
@@ -16,12 +21,15 @@ public class CalculosServiceImpl implements CalculosService {
 			int intA = Integer.parseInt(a);
 			int intB = Integer.parseInt(b);
 			int result = intA + intB;
+			tracer.trace(result);
 			respuestaMensajeDTO.setMensaje(String.valueOf(result));
 			respuestaMensajeDTO.setCodigoStatus(HttpStatus.OK.value());
 		} catch (NumberFormatException e) {
+			tracer.trace(e.getMessage());
 			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la suma.");
 			respuestaMensajeDTO.setCodigoStatus(HttpStatus.BAD_REQUEST.value());
 		} catch (Exception e) {
+			tracer.trace(e.getMessage());
 			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la suma");
 			respuestaMensajeDTO.setCodigoStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 		}
@@ -35,12 +43,15 @@ public class CalculosServiceImpl implements CalculosService {
 			int intA = Integer.parseInt(a);
 			int intB = Integer.parseInt(b);
 			int result = intA - intB;
+			tracer.trace(result);
 			respuestaMensajeDTO.setMensaje(String.valueOf(result));
 			respuestaMensajeDTO.setCodigoStatus(HttpStatus.OK.value());
 		} catch (NumberFormatException e) {
+			tracer.trace(e.getMessage());
 			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la resta.");
 			respuestaMensajeDTO.setCodigoStatus(HttpStatus.BAD_REQUEST.value());
 		} catch (Exception e) {
+			tracer.trace(e.getMessage());
 			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la resta.");
 			respuestaMensajeDTO.setCodigoStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 		}
