38608a89df3208ff3d7930ad0fe91a688ab6092a
favian <framos.ext@alianza.com.co>
2023-05-09 13:15:10 +0200

    se crean los metodos de sumar y restar.

--- calculadora/src/main/java/com/calculadora/service/impl/CalculosServiceImpl.java
+++ calculadora/src/main/java/com/calculadora/service/impl/CalculosServiceImpl.java
@@ -1,22 +1,50 @@
 package com.calculadora.service.impl;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.calculadora.dto.RespuestaMensajeDTO;
 import com.calculadora.service.CalculosService;
+
 @Service
-public class CalculosServiceImpl implements CalculosService{
+public class CalculosServiceImpl implements CalculosService {
 
 	@Override
 	public RespuestaMensajeDTO suma(String a, String b) {
-		// TODO Auto-generated method stub
-		return null;
+		RespuestaMensajeDTO respuestaMensajeDTO = new RespuestaMensajeDTO();
+		try {
+			int intA = Integer.parseInt(a);
+			int intB = Integer.parseInt(b);
+			int result = intA + intB;
+			respuestaMensajeDTO.setMensaje(String.valueOf(result));
+			respuestaMensajeDTO.setCodigoStatus(HttpStatus.OK.value());
+		} catch (NumberFormatException e) {
+			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la suma.");
+			respuestaMensajeDTO.setCodigoStatus(HttpStatus.BAD_REQUEST.value());
+		} catch (Exception e) {
+			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la suma");
+			respuestaMensajeDTO.setCodigoStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		}
+		return respuestaMensajeDTO;
 	}
 
 	@Override
 	public RespuestaMensajeDTO resta(String a, String b) {
-		// TODO Auto-generated method stub
-		return null;
+		RespuestaMensajeDTO respuestaMensajeDTO = new RespuestaMensajeDTO();
+		try {
+			int intA = Integer.parseInt(a);
+			int intB = Integer.parseInt(b);
+			int result = intA - intB;
+			respuestaMensajeDTO.setMensaje(String.valueOf(result));
+			respuestaMensajeDTO.setCodigoStatus(HttpStatus.OK.value());
+		} catch (NumberFormatException e) {
+			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la resta.");
+			respuestaMensajeDTO.setCodigoStatus(HttpStatus.BAD_REQUEST.value());
+		} catch (Exception e) {
+			respuestaMensajeDTO.setMensaje("Ocurrió un error al realizar la resta.");
+			respuestaMensajeDTO.setCodigoStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		}
+		return respuestaMensajeDTO;
 	}
 
 }
