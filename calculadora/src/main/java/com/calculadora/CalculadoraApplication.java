852dc5f49baf9a574ad1783650ecd8d1eb8b308a
favian <framos.ext@alianza.com.co>
2023-05-09 13:42:50 +0200

    se agrega el ComponentScan para que pueda encontrar la lib Trace

--- calculadora/src/main/java/com/calculadora/CalculadoraApplication.java
+++ calculadora/src/main/java/com/calculadora/CalculadoraApplication.java
@@ -2,8 +2,10 @@ package com.calculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.calculadora", "io.corp.calculator"})
 public class CalculadoraApplication {
 
 	public static void main(String[] args) {
